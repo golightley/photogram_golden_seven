040596247c219fe85400e99bb0f6ebd431fbc529
golightley <golightley@gmail.com>
2015-05-10 22:33:25 -0500

    HW
    Liam

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,55 @@ class PhotosController < ApplicationController
   end
 
 
+  def show
+    @link = Photo.find(params["id"]).source
+  end
+
+
+  def new_form
+    # @link = Photo.find(params["id"]).source
+  end
+
+  def create_photo
+    # @link = Photo.find(params["id"]).source
+  end
+
+ def create_row
+
+	  p         = Photo.new
+	  p.source  = params["the_source"]
+	  p.caption = params["the_caption"]
+	  p.save  
+
+	  @caption  = params["the_caption"]
+
+ end
+
+
+  def destroy 
+
+  		i 		  =Photo.find(params["id"])
+  		@caption  = i.caption  
+  		i.destroy
+ end
+
+ def edit_form
+
+  		i 		  =Photo.find(params["id"])
+  		@caption  = i.caption
+  		@source   = i.source
+
 end
+
+ def update_form
+
+    p = Photo.find(params[:id])
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+end
+
+end
+
+
